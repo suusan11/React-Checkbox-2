88bfe8ca5ac5f47cff21c0a6bdb601137e86e0e5
suusan11 <gawow11@gmail.com>
2019-04-15 14:46:43 -0700

    create class of checkbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'react-checkbox-tree/lib/react-checkbox-tree.css';
+
+import Widget from './CoverdAreaCheckbox';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Widget />
       </div>
     );
   }
